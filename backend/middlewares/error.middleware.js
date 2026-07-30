6cd2ef02a80d20e7dea54c4762fd693a736c88c7
Khushi pandey <Khushipandey29100@gmail.com>
2026-07-30 21:45:21 +0530

    handle duplicate email error in middleware

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -1,6 +1,12 @@
 export const errorMiddleware = (err, req, res, next) => {
     console.log("me error middleware hu",err.message)
 
+    if (err.code === 11000) {
+        return res.status(400).json({
+            message: `${Object.keys(err.keyValue)[0]} already exists`,
+        });
+    }
+    
     let status = err.statusCode  || 500;
     
     return res.status(status).json({
